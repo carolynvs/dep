8f5c751514f2945e99629545b90aa2e5d67b32c7
Carolyn Van Slyck <me@carolynvanslyck.com>
2017-05-11 14:55:16 -0500

    Add analyzer components for root and tools

---
+++ cmd/dep/compositeAnalyzer.go
@@ -0,0 +1,22 @@
+// Copyright 2016 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"errors"
+
+	"github.com/golang/dep"
+	"github.com/golang/dep/internal/gps"
+)
+
+// compositeAnalyzer overlays configuration from multiple analyzers
+type compositeAnalyzer struct {
+	// Analyzers is the set of analyzers to apply, last one wins any conflicts
+	Analyzers []rootProjectAnalyzer
+}
+
+func (a compositeAnalyzer) DeriveRootManifestAndLock(path string, n gps.ProjectRoot) (*dep.Manifest, *dep.Lock, error) {
+	return nil, nil, errors.New("Not implemented")
+}
